d022e7f8db1a3b7d394bad580583148e2d8fba24
shopify <kleysterdark@gmail.com>
2026-09-08 23:56:50 +0200

    Todos os arquivos do projeto

---
+++ app/auth30-demo/page.tsx
@@ -0,0 +1,17 @@
+﻿import { Auth30 } from '@/components/ui/auth30';
+
+export default function Auth30Demo() {
+  return (
+    <Auth30
+      heading="Enter your PIN"
+      description="Use your 4-digit PIN to unlock your account."
+      pinLength={4}
+      labels={{ biometric: 'Use biometrics', backspace: 'Delete' }}
+      forgotPrompt={{
+        text: 'Forgot your PIN?',
+        linkLabel: 'Reset it',
+        href: 'https://beste.co',
+      }}
+    />
+  );
+}
